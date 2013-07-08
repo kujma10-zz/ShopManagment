ae6cabbb16ceb84dca919d3c8d37094954e4e426
mda <jilbertaz@yahoo.com>
2013-07-08 14:42:37 +0400

    daemata magaziis nashtebis kontroleri

---
+++ ShopManagment/Controllers/BalancesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopManagment.Controllers
+{
+    public class BalancesController : Controller
+    {
+        //
+        // GET: /Balances/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
