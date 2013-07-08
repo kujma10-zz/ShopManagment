41286a7c0c7b88719ba89b3b0cfc9cd47e7a3e2b
mda <jilbertaz@yahoo.com>
2013-07-08 14:42:58 +0400

    daemata menejeris gverdis gayidvebis istoriis kontroleri

---
+++ ShopManagment/Controllers/SalesHistoryManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopManagment.Controllers
+{
+    public class SalesHistoryManagerController : Controller
+    {
+        //
+        // GET: /SalesHistoryManager/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
