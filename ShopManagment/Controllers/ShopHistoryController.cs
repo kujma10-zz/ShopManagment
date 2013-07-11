80f40c8a08a3662b85d853e70c1597260e56031b
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2013-07-11 15:43:14 +0400

    shesworda mcire shecdomebi gayidvebis gverdze. sheiqmna istoriis contoleri

--- ShopManagment/Controllers/ShopHistoryController.cs
+++ ShopManagment/Controllers/ShopHistoryController.cs
@@ -5,27 +5,24 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.Objects;
-using System.Data.Linq;
 
 namespace ShopManagment.Controllers
 {
-    [Authorize(Roles = ("ShopOperator"))]
-    public class ShopSalesController : Controller
+    public class ShopHistoryController : Controller
     {
         private ShopEntities db = new ShopEntities();
 
         //
-        // GET: /ShopSales/
+        // GET: /ShopHistory/
 
         public ActionResult Index()
         {
-            DateTime today = Convert.ToDateTime(DateTime.Today).Date;
-            return View(db.Sales.Where(s => EntityFunctions.TruncateTime(s.Date) == today).ToList());
+            var sales = db.Sales.Include(s => s.Admin).Include(s => s.Category).Include(s => s.Product).Include(s => s.Storage);
+            return View(sales.ToList());
         }
 
         //
-        // GET: /ShopSales/Details/5
+        // GET: /ShopHistory/Details/5
 
         public ActionResult Details(int id = 0)
         {
@@ -38,11 +35,11 @@ namespace ShopManagment.Controllers
         }
 
         //
-        // GET: /ShopSales/Create
+        // GET: /ShopHistory/Create
 
         public ActionResult Create()
         {
-            ViewBag.AdminID = new SelectList(db.Categories, "ID", "Name");
+            ViewBag.AdminID = new SelectList(db.Admins, "ID", "Username");
             ViewBag.CatID = new SelectList(db.Categories, "ID", "Name");
             ViewBag.ProductID = new SelectList(db.Products, "ID", "Name");
             ViewBag.StorageID = new SelectList(db.Storages, "ID", "Name");
@@ -50,47 +47,27 @@ namespace ShopManagment.Controllers
         }
 
         //
-        // POST: /ShopSales/Create
+        // POST: /ShopHistory/Create
 
         [HttpPost]
         public ActionResult Create(Sale sale)
         {
-            var adminInfo = db.Admins.Where(a => a.Username.Equals(User.Identity.Name)).First();
-            sale.AdminID = adminInfo.ID;
-            sale.Date = DateTime.Now;
-            sale.Returned = false;
             if (ModelState.IsValid)
             {
-                affectStorage(sale);
                 db.Sales.Add(sale);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
+            ViewBag.AdminID = new SelectList(db.Admins, "ID", "Username", sale.AdminID);
             ViewBag.CatID = new SelectList(db.Categories, "ID", "Name", sale.CatID);
             ViewBag.ProductID = new SelectList(db.Products, "ID", "Name", sale.ProductID);
             ViewBag.StorageID = new SelectList(db.Storages, "ID", "Name", sale.StorageID);
             return View(sale);
         }
 
-        private void affectStorage(Sale sale)
-        {
-            var a = db.Balances.Where(s=>s.StorageID == sale.StorageID && s.ProductID == sale.ProductID).First();
-            Balance b = a;
-            b.Quantity -= sale.Quantity;
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                // Provide for exceptions.
-            }
-        }
-
         //
-        // GET: /ShopSales/Edit/5
+        // GET: /ShopHistory/Edit/5
 
         public ActionResult Edit(int id = 0)
         {
@@ -107,7 +84,7 @@ namespace ShopManagment.Controllers
         }
 
         //
-        // POST: /ShopSales/Edit/5
+        // POST: /ShopHistory/Edit/5
 
         [HttpPost]
         public ActionResult Edit(Sale sale)
@@ -126,38 +103,20 @@ namespace ShopManagment.Controllers
         }
 
         //
-        // GET: /ShopSales/Delete/5
+        // GET: /ShopHistory/Delete/5
 
         public ActionResult Delete(int id = 0)
         {
             Sale sale = db.Sales.Find(id);
-            sale.Returned = true;
-            returnToStorage(sale);
             if (sale == null)
             {
                 return HttpNotFound();
             }
-            return RedirectToAction("Index");
-        }
-
-        private void returnToStorage(Sale sale)
-        {
-            var a = db.Balances.Where(s => s.StorageID == sale.StorageID && s.ProductID == sale.ProductID).First();
-            Balance b = a;
-            b.Quantity += sale.Quantity;
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                // Provide for exceptions.
-            }
+            return View(sale);
         }
 
         //
-        // POST: /ShopSales/Delete/5
+        // POST: /ShopHistory/Delete/5
 
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
@@ -172,37 +131,6 @@ namespace ShopManagment.Controllers
         {
             db.Dispose();
             base.Dispose(disposing);
-        }
-
-        public ActionResult GetListValue(String category, String product, String storage)
-        {
-            int categoryID = Convert.ToInt32(category);
-            int productID = Convert.ToInt32(product);
-            int storageID = Convert.ToInt32(storage);
-
-            var quantityResult = db.Balances.Where(b => b.CatID == categoryID && b.ProductID == productID && b.StorageID == storageID);
-            int quantity = 0;
-            if (quantityResult.Count() > 0)
-            {
-                quantity = quantityResult.First().Quantity;
-            }
-            else
-            {
-                quantity = 0;
-            }
-
-            var priceResult = db.Products.Where(p => p.ID == productID && p.CatID==categoryID);
-            double? price = 0;
-            if (priceResult.Count() > 0)
-            {
-                price = priceResult.First().Price;
-            }
-            else
-            {
-                price = 0;
-            }
-
-            return Json(new { price = price, quantity = quantity }, JsonRequestBehavior.AllowGet);
         }
     }
 }
